5517e04312471145fca3bfc40c6ac40d8247feba
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-09-06 14:28:30 +0500

    hw08-patterns fin

--- hw08-json/build.gradle.kts
+++ hw08-json/build.gradle.kts
@@ -10,11 +10,14 @@ repositories {
 
 dependencies {
     implementation("ch.qos.logback:logback-classic")
-
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core")
     testImplementation("org.mockito:mockito-junit-jupiter")
+    compileOnly ("org.projectlombok:lombok")
+    annotationProcessor ("org.projectlombok:lombok")
 }
 
 tasks.test {
--- hw08-json/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-json/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,13 +1,30 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class FileSerializer implements Serializer {
 
-    public FileSerializer(String fileName) {}
+    private static final ObjectMapper mapper = JsonMapper.builder().build();
+    private final String fileName;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
 
     @Override
     public void serialize(Map<String, Double> data) {
-        // формирует результирующий json и сохраняет его в файл
+        var file = new File(fileName);
+        try {
+            mapper.writeValue(file, data);
+        } catch (IOException e) {
+            log.error("Ошибка записи файла " + fileName + ": " + e.getMessage(), e);
+            throw new FileProcessException(e);
+        }
     }
 }
