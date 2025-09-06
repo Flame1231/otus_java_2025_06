5d463fe5fd51d86df32e5d7f630b10b0ade8899a
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-09-06 12:52:42 +0500

    hw08-patterns initial commit

---
+++ hw08-json/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "ru.otus"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.mockito:mockito-junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
