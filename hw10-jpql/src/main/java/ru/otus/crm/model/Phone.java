56326454fbcc2e617b3aaa6bafebe136a51b6b89
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-09-20 12:16:07 +0500

    hw10-jpql

---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,42 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "phone")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "number", nullable = false)
+    private String number;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    @Override
+    public Phone clone() {
+        return new Phone(this.id, this.number);
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" + "id=" + id + ", number='" + number + '\'' + '}';
+    }
+}
