48f575acb6a682bbf2f23333cc2e37920cf3255e
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-09-29 23:51:01 +0500

    hw11-cache: fin

--- hw11-cache/build.gradle.kts
+++ hw11-cache/build.gradle.kts
@@ -9,6 +9,13 @@ repositories {
 }
 
 dependencies {
+    implementation ("org.projectlombok:lombok")
+    annotationProcessor ("org.projectlombok:lombok")
+    implementation("org.hibernate.orm:hibernate-core")
+    implementation("ch.qos.logback:logback-classic")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.postgresql:postgresql")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
